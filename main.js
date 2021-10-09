a33b0cd151e5a0b85783a1d48f6cd279b4b3a924
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-09 13:49:54 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+/*The pop() method removes an element from the end of an array, while shift()
+removes an element from the beginning.*/
+
+let greetings = ['whats up?', 'hello', 'see ya!'];
+
+greetings.pop();
+// now equals ['whats up?', 'hello']
+
+greetings.shift();
+// now equals ['hello']
